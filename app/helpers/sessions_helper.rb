5e07805a3fbcbd9cb3d1afd7f35aeecef92dd4be
Henry Zhao <henrymzhao@gmail.com>
2017-11-23 19:03:01 -0800

    added satellite index page

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -8,6 +8,8 @@ module SessionsHelper
   def current_user
     @current_user ||= User.find_by(id: session[:user_id])
   end
+  helper_method :current_user
+
 
   def log_out
     session.delete(:user_id)
