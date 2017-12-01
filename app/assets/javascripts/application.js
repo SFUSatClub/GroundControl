daa5b48f8a62654f38d5c89c2b58e0907137ec18
dvenable <dvenable@sfu.ca>
2017-12-01 14:26:42 -0800

    Got map partially working

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,7 +12,7 @@
 //
 //= require jquery
 //= require jquery_ujs
-//= require bootstrap-sprockets
+//= require bootstrap-sprockets	
 //= require turbolinks
 //= require_tree .
-//= require map
+//= require map2
