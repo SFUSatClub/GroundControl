bec357faec6fb33cbbae4674f7006e83c1d4d7f6
vsoedhwa <vsoedhwa@sfu.ca>
2017-12-01 17:49:49 -0800

    increased memory in vagrantfile

--- app/assets/javascripts/map.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function() {
-  function initialize() {
-    var earth = new WE.map('earth_div');
-    WE.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(earth);
-  }
-}
