058f897536f2edad94a8a3cda5063ec9bb0f2f85
Henry Zhao <henrymzhao@gmail.com>
2017-12-03 13:30:13 -0800

    fixed routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,17 @@
 Rails.application.routes.draw do
   root 'satellites#index'
-  get 'users/:id/map', to: 'users#map'
 
   # for temporary static pages
   get  'static_pages/home'
-  
+
   # for user login/logout
   get    '/login',   to: 'sessions#new'
   post   '/login',   to: 'sessions#create'
   delete '/logout',  to: 'sessions#destroy'
 
-  resources :users
+  resources :users do
+    get :map
+  end
   resources :satellites
 
 
